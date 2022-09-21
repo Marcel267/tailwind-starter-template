44c13c728a8c8a8d4517aa218c3b01a5d552eadb
Marcel <martinez.otano2000@web.de>
2022-09-21 20:27:39 +0200

    Add dark mode toggle & more configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    extend: {
+      colors: {
+        primary: 'hsl(262, 80%, 50%)',
+        secondary: 'hsl(316, 70%, 50%)',
+        accent: 'hsl(175, 70%, 41%)',
+        neutral: 'hsl(218, 18%, 12%)',
+        base: 'hsl(220, 18%, 20%)',
+        info: 'hsl(198, 93%, 60%)',
+        success: 'hsl(158, 64%, 52%)',
+        warning: 'hsl(43, 96%, 56%)',
+        error: 'hsl(0, 91%, 71%)'
+      }
+    },
   },
   plugins: [],
 }
