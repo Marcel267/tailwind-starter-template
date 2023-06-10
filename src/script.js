4f66ec785dbf0c0d221cfadc9ce9453ba94cd6da
Marcel267 <martinez.otano2000@web.de>
2023-06-10 20:45:32 +0200

    Add svgs

--- src/script.js
+++ src/script.js
@@ -3,37 +3,36 @@
  */
 
 //icons
-// const sunIcon = document.querySelector('.sun');
-// const moonIcon = document.querySelector('.moon');
+const sunIcon = document.querySelector('.sun');
+const moonIcon = document.querySelector('.moon');
 const button = document.querySelector('.theme-button');
 
 //Icon/Text Toggling
-// const iconToggle = () => {
-//     moonIcon.classList.toggle('display-none');
-//     sunIcon.classList.toggle('display-none');
-// }
+const iconToggle = () => {
+    moonIcon.classList.toggle('hidden');
+    sunIcon.classList.toggle('hidden');
+}
 
 //Initial Theme Check
 const themeCheck = () => {
     if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
         document.documentElement.classList.add('dark');
-        // moonIcon.classList.add('.display-none');
+        moonIcon.classList.add('hidden');
         return;
     }
-    // sunIcon.classList.add('.display-none');
+    sunIcon.classList.add('hidden');
 }
 
 //Manual Theme Switch
 const themeSwitch = () => {
+    iconToggle();
     if (document.documentElement.classList.contains('dark')) {
         document.documentElement.classList.remove('dark');
         localStorage.setItem('theme', 'light');
-        // iconToggle();
         return;
     }
     document.documentElement.classList.add('dark')
     localStorage.setItem('theme', 'dark');
-    // iconToggle();
 }
 
 //call theme switch on clicking buttons
